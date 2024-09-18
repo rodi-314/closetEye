5d35174f5560246bd4d4d7ab33ed785e34ef54c4
C2linaung <c2linaung.main@gmail.com>
2024-09-19 02:52:48 +0800

    added tokencache

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,7 +3,33 @@ import { Text, View, ActivityIndicator } from 'react-native';
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { ClerkProvider, SignedIn, SignedOut, ClerkLoaded } from '@clerk/clerk-expo'; 
-import LoginScreen from './../components/LoginScreen'
+import LoginScreen from './../components/LoginScreen';
+import * as SecureStore from 'expo-secure-store'
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  },
+}
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +45,7 @@ export default function RootLayout() {
   }
   
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <ClerkLoaded>
         <SignedIn>
           <Stack screenOptions={{headerShown:false}}>
