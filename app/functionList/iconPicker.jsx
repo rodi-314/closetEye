d1a58ec5ab66dc5df6e554acd5194330a42f7cd8
C2linaung <c2linaung.main@gmail.com>
2024-09-19 19:37:21 +0800

    handleAdd in progress

--- app/functionList/iconPicker.jsx
+++ app/functionList/iconPicker.jsx
@@ -3,48 +3,49 @@ import { View, Image, TouchableOpacity, FlatList, Modal, StyleSheet } from 'reac
 
 // hardcoded
 const icons = [
-  { id: 1, uri: require('../../assets/images/ClothingIcons/shirt1.png') },
-  { id: 2, uri: require('../../assets/images/ClothingIcons/shirt2.png') },
-  { id: 3, uri: require('../../assets/images/ClothingIcons/shirt3.png') },
-  { id: 4, uri: require('../../assets/images/ClothingIcons/shirt4.png') },
-  { id: 5, uri: require('../../assets/images/ClothingIcons/dress1.png') },
-  { id: 6, uri: require('../../assets/images/ClothingIcons/dress2.png') },
-  { id: 7, uri: require('../../assets/images/ClothingIcons/dress3.png') },
-  { id: 8, uri: require('../../assets/images/ClothingIcons/shorts1.png') },
-  { id: 9, uri: require('../../assets/images/ClothingIcons/shorts2.png') },
-  { id: 10, uri: require('../../assets/images/ClothingIcons/shorts3.png') },
-  { id: 11, uri: require('../../assets/images/ClothingIcons/skirt1.png') },
-  { id: 12, uri: require('../../assets/images/ClothingIcons/skirt2.png') },
-  { id: 13, uri: require('../../assets/images/ClothingIcons/skirt3.png') },
-  { id: 14, uri: require('../../assets/images/ClothingIcons/skirt4.png') },
-  { id: 15, uri: require('../../assets/images/ClothingIcons/socks1.png') },
-  { id: 16, uri: require('../../assets/images/ClothingIcons/socks2.png') },
-  { id: 17, uri: require('../../assets/images/ClothingIcons/socks3.png') }
+  { id: 'shirt1.png', uri: require('../../assets/images/ClothingIcons/shirt1.png') },
+  { id: 'shirt2.png', uri: require('../../assets/images/ClothingIcons/shirt2.png') },
+  { id: 'shirt3.png', uri: require('../../assets/images/ClothingIcons/shirt3.png') },
+  { id: 'shirt4.png', uri: require('../../assets/images/ClothingIcons/shirt4.png') },
+  { id: 'dress1.png', uri: require('../../assets/images/ClothingIcons/dress1.png') },
+  { id: 'dress2.png', uri: require('../../assets/images/ClothingIcons/dress2.png') },
+  { id: 'dress3.png', uri: require('../../assets/images/ClothingIcons/dress3.png') },
+  { id: 'shorts1.png', uri: require('../../assets/images/ClothingIcons/shorts1.png') },
+  { id: 'shorts2.png', uri: require('../../assets/images/ClothingIcons/shorts2.png') },
+  { id: 'shorts3.png', uri: require('../../assets/images/ClothingIcons/shorts3.png') },
+  { id: 'skirt1.png', uri: require('../../assets/images/ClothingIcons/skirt1.png') },
+  { id: 'skirt2.png', uri: require('../../assets/images/ClothingIcons/skirt2.png') },
+  { id: 'skirt3.png', uri: require('../../assets/images/ClothingIcons/skirt3.png') },
+  { id: 'skirt4.png', uri: require('../../assets/images/ClothingIcons/skirt4.png') },
+  { id: 'socks1.png', uri: require('../../assets/images/ClothingIcons/socks1.png') },
+  { id: 'socks2.png', uri: require('../../assets/images/ClothingIcons/socks2.png') },
+  { id: 'socks3.png', uri: require('../../assets/images/ClothingIcons/socks3.png') }
 ];
 
-export default function iconPicker({ visible, onIconSelected, onClose }) {
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.modalView}>
-        <FlatList
-          data={icons}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => onIconSelected(item.uri)}>
-              <Image source={item.uri} style={styles.iconImage} />
-            </TouchableOpacity>
-          )}
-          keyExtractor={item => item.id.toString()}
-          numColumns={3}
-        />
-      </View>
-    </Modal>
-  );
-}
+export default function IconPicker({ visible, onIconSelected, onClose }) {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={onClose}
+      >
+        <View style={styles.modalView}>
+          <FlatList
+            data={icons}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => onIconSelected(item.uri, item.id)}>
+                <Image source={item.uri} style={styles.iconImage} />
+              </TouchableOpacity>
+            )}
+            keyExtractor={item => item.id}
+            numColumns={3}
+          />
+        </View>
+      </Modal>
+    );
+  }
+  
 
 const styles = StyleSheet.create({
   modalView: {
--- app/functionList/register.jsx
+++ app/functionList/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import { useNavigation } from 'expo-router';
 import IconPicker from './iconPicker';
 
@@ -7,6 +7,10 @@ export default function Register() {
   const navigation = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
   const [currentIcon, setCurrentIcon] = useState(require('../../assets/images/icons.png'));
+  const [iconPath,setIconPath] = useState('icons.png');
+  const [name,setName] = useState();
+  const [key,setKey] = useState();
+  const [description,setDescription] = useState();
 
   useEffect(() => {
     navigation.setOptions({
@@ -26,10 +30,15 @@ export default function Register() {
     setModalVisible(true);
   };
 
-  const handleIconSelection = (iconUri) => {
+  const handleIconSelection = (iconUri, id) => {
     setCurrentIcon(iconUri);
+    setIconPath(id)
     setModalVisible(false);
   };
+
+  const handleAdd = () => {
+    console.log("Icon Path is:", iconPath)
+  }
 
   return (
     <View style={styles.container}>
@@ -39,6 +48,21 @@ export default function Register() {
         <Image source={currentIcon} style={styles.icon} />
       </TouchableOpacity>
       <IconPicker visible={modalVisible} onIconSelected={handleIconSelection} onClose={() => setModalVisible(false)} />
+      <Text style={styles.iconText}>Pick an Icon!</Text>
+      <View>
+        <TextInput placeholder='Name' style={styles.textInput} onChangeText={(v)=> setName(v)}/>
+        <TextInput placeholder='Activation Key' style={styles.textInput} onChangeText={(v)=> setKey(v)}/>
+        <TextInput
+          placeholder='Description'
+          style={[styles.textInput, styles.descriptionInput]} 
+          multiline={true}
+          numberOfLines={3}
+          onChangeText={(v)=> setDescription(v)}
+        />
+      </View>
+      <TouchableOpacity style={styles.addContainer} onPress={handleAdd}>
+        <Text style={styles.addText}>Add New Module</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -58,6 +82,36 @@ const styles = StyleSheet.create({
   icon: {
     width: 100,
     height: 100,
-    marginTop: 20
+    marginTop: 20,
+    borderRadius:10,
+    borderColor:'grey',
+    borderWidth:2,
+  },
+  iconText:{
+    fontFamily:'outfit-medium',
+    marginLeft:13
+  },
+  textInput:{
+    padding:15,
+    borderWidth:1,
+    borderRadius:10,
+    fontSize:15,
+    backgroundColor:'white',
+    marginTop:10,
+    fontFamily:'outfit'
+  },
+  descriptionInput: {
+    height: 100 
+  },
+  addContainer:{
+    padding:15,
+    backgroundColor:'#6d5cc4',
+    borderRadius:10,
+    marginTop:20
+  },
+  addText:{
+    textAlign:'center',
+    fontFamily:'outfit-medium',
+    color:'white'
   }
 });
