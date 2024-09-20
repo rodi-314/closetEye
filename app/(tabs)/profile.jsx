b63d716fe821c09d93628954f6ae69bcb8ede164
C2linaung <c2linaung.main@gmail.com>
2024-09-21 02:27:04 +0800

    complete preliminary profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,16 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import UserIntro from '../../components/Profile/UserIntro'
 
 export default function profile() {
   return (
-    <View>
+    <View style={{
+      padding:20,
+      marginTop:10
+    }}>
       <Text style={{
-        fontSize: 20,
+        fontSize: 35,
         fontFamily:'outfit-bold',
-        color:'grey',
-        textAlign:'center',
-        marginTop:'90%'
       }}>Profile</Text>
+
+      <UserIntro/>
+
     </View>
   )
 }
---
+++ components/Profile/UserIntro.jsx
@@ -0,0 +1,56 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { TouchableOpacity } from 'react-native';
+
+export default function UserIntro() {
+  
+    const {user}=useUser();
+
+    return (
+    <View style={{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:200,
+    }}>
+      <Image source={{uri:user?.imageUrl}} style={{
+        width:80,
+        height:80,
+        borderRadius:99,
+      }}/>
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:20 
+      }}>{user?.fullName}</Text>
+      <Text style={{
+        fontFamily:'outfit',
+        fontSize:15
+      }}>{user?.primaryEmailAddress?.emailAddress}</Text>
+      <TouchableOpacity>
+        <View style={{
+            display:'flex',
+            flexDirection:'row',
+            justifyContent:'center',
+            alignItems:'center',
+            backgroundColor:'#d7d5df',
+            padding:5,
+            paddingLeft:7,
+            paddingRight:7,
+            borderRadius:10,
+            marginTop:10
+        }}>
+            <Image source={require('./../../assets/images/logout.png')} style={{
+                width:50,
+                height:50,
+            }}/>
+            <Text style={{
+                fontFamily:'outfit-medium',
+                fontSize:15,
+                marginLeft:10
+            }}>Log Out</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
