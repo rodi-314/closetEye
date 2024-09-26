daf8eb660bfe0d2a04b04f423c3992a46dccfe58
C2linaung <c2linaung.main@gmail.com>
2024-09-27 00:15:34 +0800

    edit to allow access on web also

--- hooks/useWarmUpBrowser.tsx
+++ hooks/useWarmUpBrowser.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import * as WebBrowser from "expo-web-browser";
+import { Platform } from 'react-native';
 
 export const useWarmUpBrowser = () => {
     React.useEffect(() => {
-        void WebBrowser.warmUpAsync();
+        if (Platform.OS !== 'web') {
+          WebBrowser.warmUpAsync();
+        }
         return () => {
-            void WebBrowser.coolDownAsync();
+          if (Platform.OS !== 'web') {
+            WebBrowser.coolDownAsync();
+          }
         };
-    }, []);
+      }, []);
 };
